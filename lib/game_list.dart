66477e0c5cd69790a6d556005087002ca96a59e0
Yihun Alemayehu <yankure01@gmail.com>
2024-02-07 17:53:50 +0300

    fdb-03

---
+++ lib/game_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameList extends StatelessWidget {
+  const GameList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
