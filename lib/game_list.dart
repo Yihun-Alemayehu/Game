73be17ece98980dda78b4ce57000f12ae9f18cf6
Yihun <yankure01@gmail.com>
2024-02-10 14:30:20 +0300

    fdb

--- lib/game_list.dart
+++ lib/game_list.dart
@@ -5,6 +5,11 @@ class GameList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Game list'),
+      ),
+      body: Column(),
+    );
   }
 }
